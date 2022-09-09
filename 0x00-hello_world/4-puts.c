5698853cee4eba984e6225ae991373ac01e57827
Oyelade Omotayo <oyeladeomotayo17@gmail.com>
2022-09-08 17:45:54 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/**
+/*
 
    * main - Entry point
 
@@ -10,14 +10,14 @@
 
       */
 
-int main(void)
+int main(void)   //this is the main function
 
 {
 
 
 	puts("\"Programming is like building a multilingual puzzle");
 
-		        return (0);
+	return (0);
 
 }
 
