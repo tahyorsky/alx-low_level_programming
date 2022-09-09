32d2c9e3851f7ab9540852d8d6d221a4b559c891
Oyelade Omotayo <oyeladeomotayo17@gmail.com>
2022-09-08 19:03:39 -0700

    low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
 *
 *Description: using the main function
 *this program prints "Programming is like building a multilingual puzzle
-* Return: 0
- */
+*Return: 0
+*/
 int main(void)	
 {
 	puts("\"Programming is like building a multilingual puzzle");
