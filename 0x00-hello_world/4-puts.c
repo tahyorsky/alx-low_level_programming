cd7b3231e6425ab812bf363c1d3d8043d8290925
Oyelade Omotayo <oyeladeomotayo17@gmail.com>
2022-09-08 18:18:36 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
 /*
-
 	* main -print the string in the put function
 	*this program prints "Programming is like building a multilingual puzzle
-    
-
-     * Return: always 0
-
-      */
+	* Return: always 0
+ */
 
 int main(void)
 
