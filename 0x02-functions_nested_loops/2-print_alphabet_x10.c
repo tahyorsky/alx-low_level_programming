121cbc4bdebf42d87de4f0d6beb543c76341f882
Oyelade Omotayo <oyeladeomotayo17@gmail.com>
2022-09-13 21:51:30 -0700

    tahyorsky

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,11 @@
 */
 void print_alphabet_x10(void)
 {
-	
-	char c, i;
+	char c;
 
-	for (i = 0; i <= 9; i++)
-	{
-		for (c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
