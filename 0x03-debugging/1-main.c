d995229b7ec0add06fc307ed547de40eaa12f42f
Oyelade Omotayo <oyeladeomotayo17@gmail.com>
2022-09-15 20:32:40 -0700

    Tahyorsky

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /**
 * main - causes an infinite loop
 * Return: 0
 */
+
 int main(void)
 {
 	int i;
+
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
@@ -16,5 +19,6 @@ int main(void)
 	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
+	
 	return (0);
 }
